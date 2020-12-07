753ad9b84b79eca1801900ef7a2d0f6f441bc428
Sebastian <63425540+pwn27sbx@users.noreply.github.com>
2020-12-06 23:10:01 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function contact(){
+    location.href = "https://twitter.com/Fs3b4S";
+}
+
+function contact2(){
+    location.href = "https://paypal.me/fs3b4xss?locale.x=en_US"
+}
+
+function contact3(){
+    location.href = "https://www.instagram.com/fseba_s/"
+}
+
+function contact4(){
+    location.href = "https://twitter.com/Fs3b4S"
+}
+
+console.log('hola mundo')
